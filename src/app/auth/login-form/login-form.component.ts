941cf398bf8c2f3a904e53ab5cb13f7db222f36e
eavcexe <edwingverastegui266@gmail.com>
2025-11-13 12:56:53 -0500

    Login | angular | add authComponente

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,46 +1,41 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, FormsModule]
 })
 export class LoginFormComponent {
+  email = '';
+  password = '';
 
-  // Propiedades para enviar datos al componente padre
-  // Esto debe coincidir con los nombres en el AuthFlowComponent
-  @Output() loginSubmit = new EventEmitter<any>();
-  @Output() goToRecuperar = new EventEmitter<void>();
-
-  // Variables para enlazar a los campos del formulario
-  public email = '';
-  public password = '';
-
-  constructor() { }
-
-  /**
-   * Método que se llama cuando el usuario hace clic en el botón "Acceder".
-   * Emite los datos del formulario al componente padre.
-   */
-  onSubmit() {
-    this.loginSubmit.emit({
-      email: this.email,
-      password: this.password
-    });
-    // console.log('Login data submitted:', this.email);
+  passwordVisible: boolean = false;
+  get passwordType(): string {
+    return this.passwordVisible ? 'text' : 'password';
   }
 
-  /**
-   * MÉTODO CORREGIDO:
-   * Este método es llamado por el botón "Olvidaste tu Contraseña" en el HTML.
-   * Su única tarea es activar el EventEmitter goToRecuperar.
-   */
-  onGoToRecuperar() {
+  @Output() goToRecuperar = new EventEmitter<void>();
+
+  @Output() loginSubmit = new EventEmitter<{ email: string, password: string }>();
+
+  constructor() {}
+
+
+  togglePasswordVisibility(): void {
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  onSubmit(): void {
+    if (this.email && this.password) {
+      this.loginSubmit.emit({ email: this.email, password: this.password });
+    }
+  }
+
+  onGoToRecuperar(): void {
     this.goToRecuperar.emit();
   }
 }
